4eb8010b35c7f27e7e497d5b996ea6b8c923299c
Inna Kuklina <in.d2010@yandex.ru>
2025-02-28 14:40:28 +0300

    delete 2 folders

--- spring-cloud-config-properties-client/src/main/java/org/example/ConfigClient.java
+++ spring-cloud-config-properties-client/src/main/java/org/example/ConfigClient.java
@@ -3,7 +3,6 @@ package org.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
